1ea4e8879fd307abbf6969d9e836d2b9464d20b2
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2018-11-11 15:37:18 +0000

    Another fix

--- src/main/kotlin/com/daniel/llewellyn/EntryPoint.kt
+++ src/main/kotlin/com/daniel/llewellyn/EntryPoint.kt
@@ -1,1 +1,25 @@
 package com.daniel.llewellyn
+
+import com.amazon.ask.SkillStreamHandler
+import com.amazon.ask.Skills
+import com.dan.llewellyn.ApplicationAdapter
+
+class Factory {
+
+    companion object {
+        fun getApp() : ApplicationAdapter {
+            return ApplicationAdapter(MyStoryApplication())
+        }
+    }
+}
+
+class EntryPoint : SkillStreamHandler(getSkill()) {
+
+    companion object {
+        fun getSkill() =
+                Skills.standard()
+                        .addRequestHandlers(Factory.getApp())
+                        .withSkillId(System.getenv("ALEXA_ID"))
+                        .build()
+    }
+}
